8e38c77b126f99bc5f49e0c0998c7a35bd731a5a
Aleksandr Poliakov <master@artwist.ru>
2024-02-16 22:51:29 +0300

    добавляет меню

--- app/src/main/java/com/example/actionbar/main/MainActivity.kt
+++ app/src/main/java/com/example/actionbar/main/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.actionbar.main
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.actionbar.R
 
 class MainActivity : AppCompatActivity() {
+
+    private var actionBar: ActionBar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        actionBar = supportActionBar
     }
 }
