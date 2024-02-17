a2bd745a7f9f9314af466ef86a031ecc3af2e648
Aleksandr Poliakov <master@artwist.ru>
2024-02-17 22:43:21 +0300

    добавляет механику поиска

--- app/src/main/java/com/example/actionbar/main/MainActivity.kt
+++ app/src/main/java/com/example/actionbar/main/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.actionbar.main
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import com.example.actionbar.R
 
 class MainActivity : AppCompatActivity() {
 
     private var actionBar: ActionBar? = null
     private var isBackVisible = true
+    private var namesText: TextView? = null
+    private val data = listOf("Андрей", "Иван", "Ольга", "Наталья")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +25,27 @@ class MainActivity : AppCompatActivity() {
         actionBar?.title = "Заголовок"
         actionBar?.subtitle = "Подзаголовок"
         actionBar?.setDisplayHomeAsUpEnabled(isBackVisible)
+
+        namesText = findViewById(R.id.names_TextView)
+        showNames(data)
     }
 
-    //Создание меню
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        // Обработчик событий для SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            //Функция вызывается при вводе запроса
+            override fun onQueryTextSubmit(query: String?): Boolean  = false
+
+            //Функция вызывается при изменении текста в строке поиска
+            override fun onQueryTextChange(newText: String?): Boolean {
+                showNames(data.filter { it.contains(newText.toString()) })
+                return true
+            }
+        })
         return true
     }
 
@@ -47,6 +67,10 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    fun showNames(names: List<String>){
+        namesText?.text = names.joinToString()
     }
 
 
